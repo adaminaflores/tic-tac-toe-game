3ce78626732d7e2260420cc7dd3a6f2aa50533d2
Adamina Flores <adaminaflores19@gmail.com>
2022-04-19 14:34:46 -0500

    replaced the Square class with a funtion

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,24 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props){
-        super(props);
-        this.state= {
-            value: null,
-        };
-    }
-    render() {
+function Square(props){
+  
       return (
         <button 
-        className="square" 
-        onClick={() => this.setState({value: 'X'})}
-        >
-          {this.state.value}
+        className="square" onClick={() => props.onClick()}>
+          {props.value}
         </button>
       );
     }
-  }
+  
   
   class Board extends React.Component {
       constructor(props){
@@ -28,8 +20,20 @@ class Square extends React.Component {
               squares: Array(9).fill(null),
           };
     }
+
+    handleClick(i){
+        const squares = this.state.squares.slice(i);
+        squares[i] = 'X';
+        this.setState({squares: squares})
+    }
+
     renderSquare(i) {
-      return <Square value={i}  />;
+      return (
+           <Square 
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)} 
+            />
+         );
     }
   
     render() {
